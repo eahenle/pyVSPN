efc71e02ee61036859b5185831e60011c77573d9
Adrian Henle <eahenle@gmail.com>
2021-08-01 23:52:57 -0700

    vectorize calculation

--- model_training.py
+++ model_training.py
@@ -7,19 +7,19 @@ def train(model, training_data, test_data, nb_epochs=1, stopping_threshold=0.1, 
     # Define loss function
     loss_func = torch.nn.MSELoss()
     report_epochs = nb_epochs / nb_reports
-    for i in range(nb_epochs): # train for up to `nb_epochs` cycles
+    for i in range(nb_epochs): # train for up to `nb_epochs` cycles ## TODO batch training
         loss = 0
         optimizer.zero_grad() # reset the gradients
-        for datum in training_data: ## TODO batch training, vectorization
-            y_hat = model(datum) # make prediction
-            loss += loss_func(y_hat, datum.y) # accumulate loss
-        loss /= len(training_data) # normalize loss
+        y_hat = [model(datum) for datum in training_data] # make predictions
+        y = [datum.y for datum in training_data] # pull out target values
+        losses = [loss_func(y_hat[j], y[j]) for j in range(len(y))] # calculate losses
+        loss = sum(losses) / len(training_data) # accumulate normalized losses
         if loss.item() < stopping_threshold: # evaluate early stopping ## TODO other early stopping criteria
             print("Breaking training loop at iteration {}\n".format(i))
             break
         if i % report_epochs == 0:
             print(f"Epoch\t{i}\t\t|\tLoss\t{loss}") ## TODO record on disk for viz
-        loss += l1_reg * sum([torch.sum(abs(params)) for params in model.parameters()]) # L1 weight regularization ## TODO library function? L2?
+        loss += l1_reg * sum([torch.sum(abs(params)) for params in model.parameters()]) # L1 regularization
         loss.backward() # do back-propagation to get gradients
         optimizer.step() # update weights
     y_hat_test = torch.tensor([model(x) for x in test_data])
