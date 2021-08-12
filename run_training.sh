de05b04d94f8f3188d83dd3f97052575464f611b
Adrian Henle <eahenle@gmail.com>
2021-08-11 22:04:52 -0700

    training speedup

--- run_training.sh
+++ run_training.sh
@@ -4,7 +4,7 @@
 
 python3 main.py                             \
     "working_capacity_vacuum_swing [mmol/g]"\
-    --batch_size        1                   \
+    --batch_size        512                 \
     --device            cpu                 \
     --learning_rate     0.005               \
     --l1_reg            0.01                \
