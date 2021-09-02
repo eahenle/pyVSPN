1606e632a442c900ecb64d39636ca9672aeafa2c
Adrian Henle <eahenle@gmail.com>
2021-09-02 02:03:40 -0700

    normalize losses

--- run_training.sh
+++ run_training.sh
@@ -6,11 +6,9 @@ python3 main.py                             \
     "working_capacity_vacuum_swing [mmol/g]"\
     --device            cpu                 \
     --learning_rate     0.1                 \
-    --max_epochs        10000               \
+    --max_epochs        1000                \
     --mpnn_steps        1                   \
     --hidden_encoding   10                  \
-    --stop_threshold    0.01                \
     --test_prop         0.1                 \
-    --val_prop          0.1                 \
-    --mpnn_update       mean                \
+    --val_prop          0.01                \
     --recache
