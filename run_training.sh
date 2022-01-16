dabb2764c7de48672ed6b752ebc249cf9e28db91
Adrian Henle <eahenle@gmail.com>
2022-01-16 12:04:16 -0800

    cleanup

--- run_training.sh
+++ run_training.sh
@@ -6,13 +6,13 @@ python3 main.py                             \
     "working_capacity_vacuum_swing [mmol/g]"\
     --device            cuda                \
     --learning_rate     0.01                \
-    --max_epochs        200                 \
+    --max_epochs        20                  \
     --mpnn_steps        2                   \
     --test_prop         0.1                 \
     --val_prop          0.01                \
     --batch_size        256                 \
     --lr_decay_gamma    1                   \
     --model             "ParallelVSPN"      \
-    --voro_embedding    4                   \
+    --voro_embedding    3                   \
     --random_seed       42                  \
     --recache
