5646df8a3bd4ee7eb08b2bd67f9398a2ca04b179
eahenle <eahenle@gmail.com>
2022-01-24 01:31:54 -0800

    multi-gpu

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,46 +2,44 @@
 
 # constant parameters
 model="ParallelVSPN"
-max_epochs=20
+max_epochs=2000
 test_prop=0.1
 val_prop=0.01
-batch_size=256
+batch_size=128
 random_seed=42
 learning_rate=0.01
-lr_decay_gamma=1
+mpnn_steps=3
+hidden_encoding=50
 
 # loop over tunable parameters
-for mpnn_steps in 2 3 4 5 6
+for CUDA in 0 1 2 3 # use CUDA device as source for voro_embedding
 do
-    for voro_embedding in 2 4 8 16
+    for voro_h in 10 20 30
     do
-        for voro_h in 10 20 30
-        do
-            for hidden_encoding in 30 40 50
-            do
-                echo "Running experiment w/ $mpnn_steps MPNN steps, voro embedding length $voro_embedding, hidden voro length $voro_h, hidden atom length $hidden_encoding."
+        echo "Running experiment w/ $mpnn_steps MPNN steps, voro embedding length $voro_embedding, hidden voro length $voro_h, hidden atom length $hidden_encoding."
 
-                # create experiment label string
-                str="$mpnn_steps-$voro_embedding-$voro_h-$hidden_encoding"
+        voro_embedding=$(( ($CUDA + 1) ** 2 ))
 
-                # run experiment
-                python3 main.py                             \
-                    "working_capacity_vacuum_swing [mmol/g]"\
-                    --device            cuda                \
-                    --learning_rate     $learning_rate      \
-                    --max_epochs        $max_epochs         \
-                    --mpnn_steps        $mpnn_steps         \
-                    --test_prop         $test_prop          \
-                    --val_prop          $val_prop           \
-                    --batch_size        $batch_size         \
-                    --lr_decay_gamma    $lr_decay_gamma     \
-                    --model             $model              \
-                    --voro_embedding    $voro_embedding     \
-                    --random_seed       $random_seed        \
-                    --output_path       "./tuning/$str"     \
-                    --recache
-            done
-        done
+        # create experiment label string
+        exp_code="$voro_embedding-$voro_h"
+
+        # run experiment
+        python3 main.py                             \
+            "working_capacity_vacuum_swing [mmol/g]"\
+            --device            "cuda:$CUDA"        \
+            --learning_rate     $learning_rate      \
+            --max_epochs        $max_epochs         \
+            --mpnn_steps        $mpnn_steps         \
+            --test_prop         $test_prop          \
+            --val_prop          $val_prop           \
+            --batch_size        $batch_size         \
+            --model             $model              \
+            --voro_embedding    $voro_embedding     \
+            --random_seed       $random_seed        \
+            --output_path       "./tuning/$exp_code"\
+            --voro_h            $voro_h             \
+            --hidden_encoding   $hidden_encoding    \
+            --recache
     done
 done
 
